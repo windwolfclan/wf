854a06dfe784c4809866c07212a33cc61e7b86ce
windwolfclan <119798446+windwolfclan@users.noreply.github.com>
2022-12-28 01:00:44 +0900

    rastertek - implements gaussian kernel

--- rastertek/Utility.cpp
+++ rastertek/Utility.cpp
@@ -6,6 +6,20 @@ namespace wf
 {
 	namespace utility
 	{
+		HRESULT CreateDynamicWriteBuffer( ID3D11Device* _device, ID3D11Buffer*& _buffer, UINT _ByteWidth )
+		{
+			D3D11_BUFFER_DESC buffer_desc;
+			ZeroMemory( &buffer_desc, sizeof( buffer_desc ) );
+			buffer_desc.Usage = D3D11_USAGE_DYNAMIC;
+			buffer_desc.ByteWidth = _ByteWidth;
+			buffer_desc.BindFlags = D3D11_BIND_CONSTANT_BUFFER;
+			buffer_desc.CPUAccessFlags = D3D11_CPU_ACCESS_WRITE;
+			buffer_desc.MiscFlags = 0;
+			buffer_desc.StructureByteStride = 0;
+
+			return _device->CreateBuffer( &buffer_desc, nullptr, &_buffer );
+		}
+
 		HRESULT CreateWrapSampler( ID3D11Device* _device, ID3D11SamplerState*& _sampler_state )
 		{
 			D3D11_SAMPLER_DESC sampler_desc{};
@@ -47,19 +61,33 @@ namespace wf
 
 			return _device->CreateSamplerState( &sampler_desc, &_sampler_state );
 		}
-
-		HRESULT CreateDynamicWriteBuffer( ID3D11Device* _device, ID3D11Buffer*& _buffer, UINT _ByteWidth )
+		
+		std::vector<float> CreateGaussianKernel( const size_t length )
 		{
-			D3D11_BUFFER_DESC buffer_desc;
-			ZeroMemory( &buffer_desc, sizeof( buffer_desc ) );
-			buffer_desc.Usage = D3D11_USAGE_DYNAMIC;
-			buffer_desc.ByteWidth = _ByteWidth;
-			buffer_desc.BindFlags = D3D11_BIND_CONSTANT_BUFFER;
-			buffer_desc.CPUAccessFlags = D3D11_CPU_ACCESS_WRITE;
-			buffer_desc.MiscFlags = 0;
-			buffer_desc.StructureByteStride = 0;
+			assert( length % 2 == 1 );
 
-			return _device->CreateBuffer( &buffer_desc, nullptr, &_buffer );
+			std::vector<float> kernel;
+			kernel.resize( length, 0.0 );
+
+			const double sigma{ 1.0 };
+			const double s{ 2.0 * sigma * sigma };
+			double sum{ 0.0 };
+
+			int half = static_cast<int>( length ) / 2;
+			for ( int i = -half; i <= half; ++i )
+			{
+				double r = sqrt( i * i );
+
+				kernel[ i + half ] = ( exp( -( r * r ) / s ) ) / ( XM_PI * s );
+				sum += kernel[ i + half ];
+			}
+
+			for ( size_t i = 0; i < length; ++i )
+			{
+				kernel[ i ] /= sum;
+			}
+
+			return kernel;
 		}
 
 	}
--- rastertek/Utility.h
+++ rastertek/Utility.h
@@ -7,5 +7,7 @@ namespace wf
 		HRESULT CreateDynamicWriteBuffer( ID3D11Device* _device, ID3D11Buffer*& _buffer, UINT _ByteWidth );
 		HRESULT CreateWrapSampler( ID3D11Device* _device, ID3D11SamplerState*& _sampler_state );
 		HRESULT CreateClampSampler( ID3D11Device* _device, ID3D11SamplerState*& _sampler_state );
+
+		std::vector<float> CreateGaussianKernel( const size_t length );
 	}
 }
